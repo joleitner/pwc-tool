dcba1224347d74fed2fb01bef630d25af90c9ba4
Jonas <joleitner@protonmail.com>
2024-12-12 16:52:18 +0100

    try flask instead for serverless function

--- api/index.py
+++ api/index.py
@@ -1,15 +1,6 @@
-from typing import Union
+from flask import Flask
+app = Flask(__name__)
 
-from fastapi import FastAPI
-
-app = FastAPI(docs_url="/api/docs",  redoc_url="/api/redoc", openapi_url="/api/openapi.json")
-
-
-@app.get("/api")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/api/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.route("/api/")
+def hello_world():
+    return {"message": "Hello, World!"}
